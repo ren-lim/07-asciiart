a13b6a81bf81eca4b7c1c87a0fd4364129057d6c
ren-lim <ren.lim@edu.esiee.fr>
2025-12-16 21:37:40 +0000

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #### Imports et définition des variables globales
-
+"""Module pour encoder une chaîne de caractères selon un algorithme"""
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
@@ -9,7 +9,7 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +17,22 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
+    if not s:
+        return []
+    # Initialisation
+    caractere = [s[0]]
+    occurences = [1]
+    for elem in s[1:]:
+        if elem == caractere[-1]:
+            occurences[-1] += 1
+        else:
+            caractere.append(elem)
+            occurences.append(1)
+    return list(zip(caractere, occurences))
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,22 +40,18 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
+    if not s:
+        return []
+    first_char = s[0]
+    compteur = 1
+    while compteur < len(s) and s[compteur] == first_char:
+        compteur += 1
+    return [(first_char, compteur)] + artcode_r(s[compteur:])
 
 #### Fonction principale
-
-
 def main():
+    """fonction principale pour tester les deux algorithmes"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
-
 if __name__ == "__main__":
     main()
